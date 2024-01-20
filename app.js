73a449b4cfa631834ef09246cc4ed85eadb28587
seoyuncho <103191590+seoyuncho@users.noreply.github.com>
2024-01-20 18:02:51 +0900

    download 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
+const downloadRoutes = require('./routes/downloadRoutes');
 
 const app = express();
 const port = 5000;
@@ -19,6 +20,7 @@ app.use(cors({
 
 app.use('/auth', authRoutes);
 app.use('/upload', uploadRoutes);
+app.use('/download', downloadRoutes);
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
--- routes/downloadRoutes.js
+++ routes/downloadRoutes.js
@@ -3,23 +3,27 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/file/:filename', (req, res) => {
-  try {
-    const filename = req.params.filename;
-    const directoryPath = path.join(__dirname, '../uploads');
-    const filePath = path.join(directoryPath, filename);
-
-    res.download(filePath, filename, (err) => {
-      if (err) {
-        res.status(500).send({
-          message: "파일을 다운로드하는 데 실패했습니다.",
-          error: err
+    try {
+        const filename = req.params.filename;
+        console.log("요청된 파일 이름:", filename); // 파일 이름 로그
+  
+        const directoryPath = path.join(__dirname, '../uploads');
+        const filePath = path.join(directoryPath, filename);
+        console.log("계산된 파일 경로:", filePath); // 파일 경로 로그
+  
+        res.download(filePath, filename, (err) => {
+            if (err) {
+                console.error("다운로드 중 오류 발생:", err); // 다운로드 오류 로그
+                res.status(500).send({
+                    message: "파일을 다운로드하는 데 실패했습니다.",
+                    error: err
+                });
+            }
         });
-      }
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("서버 오류");
-  }
+    } catch (err) {
+        console.error("라우트 처리 중 오류 발생:", err); // catch 블록 오류 로그
+        res.status(500).send("서버 오류");
+    }
 });
 
 module.exports = router;
